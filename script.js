c0f56a3bd78601dcf3c8396a222d9573e51ceb9c
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-03-26 16:17:31 +0100

    final commit

--- script.js
+++ script.js
@@ -37,3 +37,42 @@ function validatePhone(){
     phoneError.innerHTML = 'G';
     return true;
 }
+
+function validateEmail(){
+    var email = document.getElementById('contact-email').value;
+
+    if(email.length == 0){
+        emailError.innerHTML = 'Email is required'
+        return false;
+    }
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML = 'Email Invalid'
+        return true;
+    }
+
+    emailError.innerHTML = 'G';
+    return true;
+
+}
+
+function validateMessage(){
+var message = document.getElementById('contact-message').value;
+var required = 30;
+var left = required - message.length;
+
+if(left>0){
+    messageError.innerHTML = left + ' more character required';
+    return false
+}
+ messageError.innerHTML = 'G'
+ return true
+}
+
+function validateForm(){
+    if (!validateEmail() || !validateForm() || validateMessage() || validateName() || validatePhone){
+        submitError.style.display = 'block'
+        submitError.innerHTML = 'Please fix error!'
+        setTimeout( function(){submitError.style.display = 'none';}, 3000)
+        return false
+    }
+}
